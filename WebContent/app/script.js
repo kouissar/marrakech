002d2fc3d78bd352c52d1f70bf556ea82c9bbcc8
kouissar <kouissar@gmail.com>
2014-12-23 17:58:53 -0500

    Added login view and sample search with filter

--- WebContent/app/script.js
+++ WebContent/app/script.js
@@ -12,17 +12,19 @@ finderApp.config([ '$routeProvider', function($routeProvider) {
 	}).when('/contact', {
 		templateUrl : 'app/components/contact/contact.html',
 		controller : 'contactController'
+	}).when('/login', {
+		templateUrl : 'app/components/login/login.html',
+		controller : 'loginController'
 	}).when('/home', {
 		templateUrl : 'index.html',
 		controller : 'mainController'
 	}).when('/result', {
-		templateUrl : 'app/components/home/home.html',
+		templateUrl : 'app/components/results/resultsTable.html',
 		controller : 'homeController'
 	}).otherwise({
 		redirectTo : '/'
 	});
 } ]);
-
 
 // create the controller and inject Angular's $scope
 finderApp.controller('mainController', function($scope) {
@@ -30,30 +32,66 @@ finderApp.controller('mainController', function($scope) {
 	$scope.message = 'Welcome!';
 
 });
+finderApp.controller('loginController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Welcome!';
 
-finderApp.controller('aboutController', function($scope) {
-	$scope.message = 'Trying to make this work, if it does, it will be awesome!';
 });
+
+finderApp.controller(
+				'aboutController',
+				function($scope) {
+					$scope.message = 'Trying to make this work, if it does, it will be awesome!';
+				});
 
 finderApp.controller('contactController', function($scope) {
 	$scope.message = 'Contact us! JK. This is just a demo.';
 });
 
-finderApp.controller('homeController', function($scope) {
-	$scope.message = 'Trying to make this work, if it does, it will be awesome!';
-	this.places = masjid;
-	
+//finderApp
+//		.controller(
+//				'homeController',
+//				function($scope) {
+//	
+//				});
+
+finderApp.controller('homeController', function ($scope) {
+	  $scope.places = [
+	    {'name': 'ISG',
+	     'comment': 'full service Masjid',
+	     'code': '20876'},
+	    {'name': 'ICM',
+	     'comment': 'full service Masjid',
+	     'code': '20870'},
+	    {'name': 'Adam Center',
+	     'comment': 'full service Masjid',
+	    'code': '20800'}
+	  ];
+	});
+
+
+finderApp.controller('tabController', function() {
+	this.tab = 1;
+
+	this.setTab = function(newValue) {
+		this.tab = newValue;
+	};
+
+	this.isSet = function(tabName) {
+		return this.tab === tabName;
+	};
 });
-//test data
-var masjid = [
-{
-		name: 'Islamic Society Of Germantown',
-		zipcode: '20876',
-		address: '100 blunt rd Germantown, MD 20876',
-},
-{
-		name: 'Islamic Civil of Maryland',
-		zipcode: '20860',
-		address: '100 main rd Gaithersburg, MD 20860',
-}
-];
+
+// test data
+var masjid = [ {
+	name : 'Islamic Society Of Germantown',
+	desc : 'Full service Masjid',
+	zipcode : '20876',
+	address : '100 blunt rd Germantown, MD 20876',
+}, {
+	name : 'Islamic Civil of Maryland',
+	desc : 'Full service Masjid',
+	zipcode : '20860',
+	address : '100 main rd Gaithersburg, MD 20860',
+} ];
+
