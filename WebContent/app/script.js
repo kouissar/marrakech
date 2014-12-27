81783be2089c82a0aeba9b1b12b9c469db7dd2d2
kouissar <kouissar@gmail.com>
2014-12-27 14:38:09 -0500

    more enhancements
    adding weather and prayer times support

--- WebContent/app/script.js
+++ WebContent/app/script.js
@@ -9,15 +9,18 @@ finderApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/about', {
 		templateUrl : 'app/components/about/about.html',
 		controller : 'aboutController'
-	}).when('/contact', {
-		templateUrl : 'app/components/contact/contact.html',
-		controller : 'contactController'
+	}).when('/weather', {
+		templateUrl : 'app/components/weather/weather.html',
+		controller : 'weatherController'
 	}).when('/login', {
 		templateUrl : 'app/components/login/login.html',
 		controller : 'loginController'
 	}).when('/home', {
 		templateUrl : 'index.html',
 		controller : 'mainController'
+	}).when('/salat', {
+		templateUrl : 'app/components/salat/salat.html',
+		controller : 'salatController'
 	}).when('/result', {
 		templateUrl : 'app/components/results/resultsTable.html',
 		controller : 'homeController'
@@ -38,37 +41,50 @@ finderApp.controller('loginController', function($scope) {
 
 });
 
-finderApp.controller(
+finderApp
+		.controller(
 				'aboutController',
 				function($scope) {
 					$scope.message = 'Trying to make this work, if it does, it will be awesome!';
 				});
 
-finderApp.controller('contactController', function($scope) {
-	$scope.message = 'Contact us! JK. This is just a demo.';
+finderApp
+.controller(
+		'salatController',
+		function($scope) {
+			$scope.message = 'Trying to make this work, if it does, it will be awesome!';
+			var date = new Date(); // today
+			var times = prayTimes.getTimes(date, [39, -77], -5);
+			var list = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Midnight'];
+
+			var html = '<table id="timetable">';
+			html += '<tr><th colspan="2">'+ date.toLocaleDateString()+ '</th></tr>';
+			for(var i in list)	{
+				html += '<tr><td>'+ list[i]+ '</td>';
+				html += '<td>'+ times[list[i].toLowerCase()]+ '</td></tr>';
+			}
+			html += '</table>';
+			document.getElementById('table').innerHTML = html;
+		});
+
+
+finderApp.controller('homeController', function($scope) {
+	$scope.places = [ {
+		'abr' : 'ISG',
+		'name' : 'Islamic Society Of Germantown',
+		'address' : '839 blunt rd Germantown MD 20876',
+		'phone'   : '389-909-0091',
+		'comment' : 'full service Masjid',
+		'zipcode' : '20876'
+	}, {
+		'abr' : 'ICM',
+		'name' : 'Islamic Community of Maryland',
+		'address' : '839 blunt rd Gaithersburg MD 20876',
+		'phone'   : '389-909-0091',
+		'comment' : 'full service Masjid',
+		'zipcode' : '20800'
+	} ];
 });
-
-//finderApp
-//		.controller(
-//				'homeController',
-//				function($scope) {
-//	
-//				});
-
-finderApp.controller('homeController', function ($scope) {
-	  $scope.places = [
-	    {'name': 'ISG',
-	     'comment': 'full service Masjid',
-	     'code': '20876'},
-	    {'name': 'ICM',
-	     'comment': 'full service Masjid',
-	     'code': '20870'},
-	    {'name': 'Adam Center',
-	     'comment': 'full service Masjid',
-	    'code': '20800'}
-	  ];
-	});
-
 
 finderApp.controller('tabController', function() {
 	this.tab = 1;
@@ -82,16 +98,30 @@ finderApp.controller('tabController', function() {
 	};
 });
 
+function weatherController($scope, $http) {
+	var zip = "20004";
+	var base = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
+	var unit = "imperial";
+	//var zip  = firstName;
+	var days = "7";
+	var url = base + zip + ",USA&mode=json&units=" + unit + "&cnt=" + days;
+	// "http://api.openweathermap.org/data/2.5/forecast/daily?q=20876,USA&mode=json&units=imperial&cnt=7"
+	$http.get(url).success(function(response) {
+		$scope.names = response;
+	});
+}
+
+
+
+
+finderApp.filter('timestampToDate', function() {
+	return function(timestamp) {
+		var date = new Date(timestamp * 1000);
+		var dateObject = date.getFullYear() + '/'
+				+ ('0' + (date.getMonth() + 1)).slice(-2) + '/'
+				+ ('0' + date.getDate()).slice(-2);
+		return dateObject;
+	};
+});
 // test data
-var masjid = [ {
-	name : 'Islamic Society Of Germantown',
-	desc : 'Full service Masjid',
-	zipcode : '20876',
-	address : '100 blunt rd Germantown, MD 20876',
-}, {
-	name : 'Islamic Civil of Maryland',
-	desc : 'Full service Masjid',
-	zipcode : '20860',
-	address : '100 main rd Gaithersburg, MD 20860',
-} ];
 
